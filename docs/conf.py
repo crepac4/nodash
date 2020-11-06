82ffaf8219bc3e4edc94b061d6ca1d56d45bb854
crepac4 <you@example.com>
2020-11-06 17:47:04 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = '2020, Crepac4'
 author = 'Crepac4'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release =  '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
